a35784b9011c8a8f4d723c9e512d9f68d859d7c0
Rails Udemy Course <railsudemycourse@gmail.com>
2018-02-05 19:41:08 +0000

    Finished all things

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,10 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
   resources :user_stocks, only: [:create, :destroy]
-  get 'my_friends', to: "user#my_friends"
+  resources :users, only: [:show]
+  resources :friendships
+  get 'my_friends', to: "users#my_friends"
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
+  
 end
